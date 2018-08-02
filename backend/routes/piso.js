3972a71fe251ba94f3331b833100adc2bf2e6369
Jesus Martinez <jesmart1969@gmail.com>
2018-08-02 21:34:55 +0200

    2do commit dia 4

--- backend/routes/piso.js
+++ backend/routes/piso.js
@@ -6,6 +6,7 @@ const User = require("../models/User");
 const Piso = require("../models/Piso");
 
 pisoRoutes.get("/", (req, res, next) => {
+  console.log('aaaaaaa')
 
   Piso.find({}).then(listaPisos => {
     if (listaPisos === null) {
@@ -63,8 +64,64 @@ pisoRoutes.get("/:id", (req, res, next) => {
       })
 
 });
+/*
+idUser: { type : Schema.Types.ObjectId, ref: 'User' },
+proposito: { type: String, enum: ['venta', 'alquiler'] },
+contacto:  { nombre: String,
+             telefono: String,
+             email: String} ,
+direccion: { provincia: String,
+              localidad: String,
+              calle: String,
+              numero: Number,
+              planta: Number,
+              numPiso: String} ,
+location: {
+            type: {
+                    type: String
+                },
+            coordinates: [Number]
+            },
+fotos: [{type: String}],
+precio: Number,
+descripcion: String,
+tipo: { type: String, enum: ['piso', 'casa', 'duplex', 'atico'] },
+caracteristicas: { tamanom2: Number,
+                    numHab: Number,
+                    numBan: Number},
+fechaPublicacion: Date,
+*/
 
+pisoRoutes.post("/add", (req, res, next) => {
+  const fechaPublicacion = new Date();
+  const idUser = req.user._id;
+  const { proposito, contacto, direccion, location, fotos, 
+          precio, descripcion, tipo, caracteristicas} = req.body;
+ 
+  // if (username === "" || password === "") {
+  //   res.status(400).json({ message: 'Usuario o Contraseña vacía' });
+  //   return;
+  // }
 
+  // User.findOne({ username }, "username", (err, user) => {
+  //   if (user !== null) {
+  //     res.status(400).json({ message: 'El nombre de usuario ya existe' });
+  //     return;
+  //   }
+
+    const newPiso = new Piso({
+      idUser, proposito, contacto, direccion, location, fotos, 
+      precio, descripcion, tipo, caracteristicas, fechaPublicacion
+    });
+    console.log(`Piso  a crear: ${newPiso}`)
+    newPiso.save((err) => {
+      if (err) {
+        res.status(400).json({ message: 'Algo ha ido mal con la creación del usuario' });
+      } else{
+        res.status(200).json(newPiso);       
+      }
+    });
+  });
 
 
 module.exports = pisoRoutes;
